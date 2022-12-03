8bcc78d424555f06b97e9b9ebb8fe3252743fe7a
Aditya Gupta <aditya3492gupta@gmail.com>
2022-12-03 13:12:56 +0530

    nav bar

--- dashboard.js
+++ dashboard.js
@@ -1,13 +1,3 @@
-const list1 = document.querySelectorAll('.list1');
-function activeLink() {
-    list1.forEach((item) =>
-        item.classList.remove('active'));
-    this.classList.add('active');
-}
-list1.forEach((item) =>
-    item.addEventListener('click', activeLink));
-
-
 const list = document.querySelectorAll('.list');
 function activeLink() {
     list.forEach((item) =>
